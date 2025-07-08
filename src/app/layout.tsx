07cbb5e7608b11e9ea129945d240d040f35595d6
Team Services <abdulwahab10101c@gmail.com>
2025-07-08 14:12:19 +0000

    Console Error
    Hydration failed because the server rendered HTML didn't

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
           rel="stylesheet"
         ></link>
       </head>
-      <body className="font-body antialiased">
+      <body className="font-body antialiased" suppressHydrationWarning>
         {children}
         <Toaster />
       </body>
